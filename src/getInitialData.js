1f8aae04a63b5e50bc7775c21424bf7e3b78c85c
Nick Echols <nicke@scribd.com>
2022-10-03 09:57:27 -0700

    fix: adapt to changing table structure

--- src/getInitialData.js
+++ src/getInitialData.js
@@ -1,9 +1,9 @@
 export default function getInitialData() {
   const table = document.querySelector(".tableView-content-wrap table");
 
-  const [_, ...duties] = [...table.querySelectorAll("th")].map((el) =>
-    el.innerText.trim()
-  );
+  const [_, ...duties] = [
+    ...table.querySelector("tr").querySelectorAll("td"),
+  ].map((el) => el.innerText.trim());
 
   const lastMonthRow = [...table.querySelectorAll("tr")].pop();
 
